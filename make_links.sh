6082fd11a28ef83f05059d7b78e3393cef794cc9
Tomislav Jovanovic <tjovanovic994@gmail.com>
2016-10-30 01:02:06 +0100

    Updated shell script to include wallpapers

--- make_links.sh
+++ make_links.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ln -s /home/tjovanovic/.dotfiles/Zshrc /home/tjovanovic/.zshrc
-ln -s /home/tjovanovic/.dotfiles/Gitconfig /home/tjovanovic/.gitconfig
+ln -s /home/$USER/.dotfiles/Zshrc /home/$USER/.zshrc
+ln -s /home/$USER/.dotfiles/Gitconfig /home/$USER/.gitconfig
+ln -s /home/$USER/.dotfiles/wallpaper.jpg /home/$USER/.wall.jpg
 
 
