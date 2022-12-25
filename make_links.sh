31df6de77cdc5ed4329acf61e897483696ca95d2
Tomislav Jovanovic <tjovanovic994@gmail.com>
2022-12-25 14:10:37 +0000

    chore(make_links): make work only stuff more apparent

--- make_links.sh
+++ make_links.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ln -s /home/$USER/.dotfiles/Gitconfig /home/$USER/.gitconfig
-ln -s /home/$USER/.dotfiles/wallpaper.jpg /home/$USER/.wall.jpg
 ln -s /home/$USER/.dotfiles/fish /home/$USER/.config/fish
+
+# Work PC only --- uncomment lines below
+# ln -s /home/$USER/.dotfiles/wallpaper.jpg /home/$USER/.wall.jpg
 
 
